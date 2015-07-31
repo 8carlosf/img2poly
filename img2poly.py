9b2d95a751f576be54096d18ce1c964ccfb988c0
carlosf <8carlosf@gmail.com>
2015-07-31 23:43:06 +0100

    numpy all the way!

--- img2poly.py
+++ img2poly.py
@@ -1,33 +1,29 @@
 import sys
 import cv2
-import numpy
-from matplotlib import pyplot as plot
-from math import ceil
+import numpy as np
+import random as rand
+import matplotlib.pyplot as plt
 
-def main():
-	
-	img_path = sys.argv[1]
-	point_ratio = float(sys.argv[2])
+def main():	
+    img_path = sys.argv[1]
+    n_points = int(sys.argv[2])
+    # validate inputs (check if we have enough points in critical places!)
 
-	img = cv2.imread(img_path, 0)
-	canny = cv2.Canny(img, 50, 150)
+    img = cv2.imread(img_path, 0)
+    canny = cv2.Canny(img, 100, 200)
 
-	plot.subplot(121), plot.imshow(canny, cmap = 'gray')
+    plt.imshow(canny, cmap = 'gray')
+    #plt.show()
 
-	edges = []
-	points = []
-	for i, k in enumerate(canny):
-		for j, v in enumerate(k):
-			if(v == 255):
-				edges.append((i, j))
+    edges = np.transpose(np.nonzero(canny))
 
-	n_points = int(ceil(len(edges) * point_ratio))
-	gap = int(len(edges) / n_points)
-	ind = 0
+    np.random.shuffle(edges)
+    points = edges[:n_points]
 
-	while(ind < len(edges) and len(points) < n_points):
-		points.append(edges[ind])
-		ind += gap
+    img_points = np.zeros((len(canny), len(canny[0])))
+    img_points[zip(*points)] = 255
+    plt.imshow(img_points, cmap = 'gray')
+    plt.show()
 
 if __name__ == "__main__":
-	main()
+    main()
